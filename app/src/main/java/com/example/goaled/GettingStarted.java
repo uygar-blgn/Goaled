4e209fa1eae497e21c3043a1f202688f9bacda33
Mehmet Rodi Aydogdu <rodi.aydogdu@ug.bilkent.edu.tr>
2023-04-30 19:35:37 +0300

    Created a main activity with a navigation bar

--- app/src/main/java/com/example/goaled/GettingStarted.java
+++ app/src/main/java/com/example/goaled/GettingStarted.java
@@ -2,7 +2,9 @@ package com.example.goaled;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class GettingStarted extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class GettingStarted extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.getting_started);
+    }
+
+    public void onClick(View v){
+        Intent i = new Intent(this, MainActivity.class);
+        startActivity(i);
     }
 }
