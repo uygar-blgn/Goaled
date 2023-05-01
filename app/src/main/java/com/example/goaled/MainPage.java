cea262263d9f48190c68214e14fe6e4c9a4da0a1
uygar-blgn <uygarblgn13@outlook.com>
2023-05-01 16:10:53 +0300

    added fragments for navigation

--- app/src/main/java/com/example/goaled/MainPage.java
+++ app/src/main/java/com/example/goaled/MainPage.java
@@ -1,18 +1,57 @@
 package com.example.goaled;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainPage extends AppCompatActivity {
+
+    BottomNavigationView bottomNavBar;
+
+    UserAccomplishFragment accomplishFragment = new UserAccomplishFragment();
+    UserActivitiesFragment activitiesFragment = new UserActivitiesFragment();
+    UserGoalsFragment goalsFragment = new UserGoalsFragment();
+    UserProfileFragment profileFragment = new UserProfileFragment();
+    UserStatsFragment statsFragment = new UserStatsFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_page);
-        BottomNavigationView bottomNavBar = findViewById(R.id.bottom_navigation);
+
+        bottomNavBar = findViewById(R.id.bottom_navigation);
         bottomNavBar.setItemIconTintList(null);
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, profileFragment).commit();
+        bottomNavBar.setSelectedItemId(R.id.profile);
+
+        bottomNavBar.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem item) {
+
+                switch (item.getItemId()) {
+                    case R.id.accomplish:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, accomplishFragment).commit();
+                        return true;
+                    case R.id.activities:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, activitiesFragment).commit();
+                        return true;
+                    case R.id.goals:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, goalsFragment).commit();
+                        return true;
+                    case R.id.profile:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, profileFragment).commit();
+                        return true;
+                    case R.id.stats:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, statsFragment).commit();
+                        return true;
+                }
+                return false;
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/goaled/UserGoalsFragment.java
@@ -0,0 +1,19 @@
+package com.example.goaled;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class UserGoalsFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_user_goals, container, false);
+    }
+}
