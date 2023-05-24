5de3f6866f8349111637eda2cc32aaf27573b77b
uygar-blgn <uygarblgn13@outlook.com>
2023-05-24 09:19:51 +0300

    added ui for goals fragment, add goal 1, and add goal with no activity

---
+++ app/src/main/java/com/example/goaled/AddGoalNoActivity.java
@@ -0,0 +1,14 @@
+package com.example.goaled;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddGoalNoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_goal_no);
+    }
+}
