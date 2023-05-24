7b2b811931e00e3a4a7b948bcda9b2e9f360a064
uygar-blgn <uygarblgn13@outlook.com>
2023-05-24 21:13:56 +0300

    added ui for accomplish and add goal with activity screens

---
+++ app/src/main/java/com/example/goaled/Accomplish.java
@@ -0,0 +1,14 @@
+package com.example.goaled;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Accomplish extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_accomplish);
+    }
+}
---
+++ app/src/main/java/com/example/goaled/AddGoalWithActivity.java
@@ -0,0 +1,14 @@
+package com.example.goaled;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddGoalWithActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_goal_with);
+    }
+}
