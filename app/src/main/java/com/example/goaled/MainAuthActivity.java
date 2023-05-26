a9a336c098ae3e6050d4316cd1da06e4f640b215
Mehmet Emin Avsar <avsarmehmetemin@gmail.com>
2023-05-26 17:10:12 +0300

    Testing types for new UserLocal constructor

--- app/src/main/java/com/example/goaled/MainAuthActivity.java
+++ app/src/main/java/com/example/goaled/MainAuthActivity.java
@@ -117,7 +117,7 @@ public class MainAuthActivity extends AppCompatActivity implements View.OnClickL
                     DatabaseReference rootRef = FirebaseDatabase.getInstance().getReference("Users");
                     DatabaseReference userRef = rootRef.child(firebaseUser.getUid());
 
-                    UserLocal userLocal = new UserLocal();
+                    UserLocal userLocal;
 
 
 
@@ -135,9 +135,9 @@ public class MainAuthActivity extends AppCompatActivity implements View.OnClickL
                                 double totalPI;
                                 HashMap<String, Double> userStats;
                                 HashMap<String, Double> statMultipliers;
-                                ArrayList<UserAccomplishment> allAccomplishments;
-                                ArrayList<UserGoal> allGoals;
-                                ArrayList<UserActivity> allActivities;
+                                ArrayList<HashMap<String,?>> allAccomplishments;
+                                ArrayList<HashMap<String,?>> allGoals;
+                                ArrayList<HashMap<String,?>> allActivities;
                                 String Uid;
 
                                 email = (String) snapshot.child("email").getValue();
@@ -150,10 +150,12 @@ public class MainAuthActivity extends AppCompatActivity implements View.OnClickL
                                 Uid = (String) snapshot.child("uid").getValue();
                                 userStats = (HashMap<String, Double>) snapshot.child("userStats").getValue();
                                 statMultipliers = (HashMap<String, Double>) snapshot.child("statMultipliers").getValue();
-                                allAccomplishments = (ArrayList<UserAccomplishment> ) snapshot.child("allAccomplishments").getValue();
-                                allGoals = (ArrayList<UserGoal>) snapshot.child("allGoals").getValue();
-                                allActivities = (ArrayList<UserActivity>) snapshot.child("allActivities").getValue();
+                                allAccomplishments = (ArrayList<HashMap<String,?>> ) snapshot.child("allAccomplishments").getValue();
+                                allGoals = (ArrayList<HashMap<String,?>>) snapshot.child("allGoals").getValue();
+                                allActivities = (ArrayList<HashMap<String,?>>) snapshot.child("allActivities").getValue();
 
+                                userLocal = new UserLocal(email, fullName, age, level, xp, xpForNextLevel, totalPI,
+                                        Uid, userStats, statMultipliers, allAccomplishments, allGoals, allActivities);
 
                             }
 
