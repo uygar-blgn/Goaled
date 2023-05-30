3241d715554b6d6a444fdff1042b2267565a69b8
uygar-blgn <uygarblgn13@outlook.com>
2023-05-30 23:03:05 +0300

    Class stuff done

--- app/src/main/java/com/example/goaled/PickClass.java
+++ app/src/main/java/com/example/goaled/PickClass.java
@@ -1,22 +1,70 @@
 package com.example.goaled;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 public class PickClass extends AppCompatActivity {
     private UserLocal userLocal;
+    private String chosenClass;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pick_class);
 
         userLocal = (UserLocal) getIntent().getSerializableExtra("USER");
+
+        CardView c1 = findViewById(R.id.legoal);
+        CardView c2 = findViewById(R.id.cardView2);
+        CardView c3 = findViewById(R.id.cardView3);
+        CardView c4 = findViewById(R.id.cardView4);
+        CardView c5 = findViewById(R.id.cardView);
+        TextView tw = findViewById(R.id.textView4);
+
+        c1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                chosenClass = "Strength";
+                tw.setText("Chosen Class: " + chosenClass);
+            }
+        });
+        c2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                chosenClass = "Intellect";
+                tw.setText("Chosen Class: " + chosenClass);
+            }
+        });
+        c3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                chosenClass = "Endurance";
+                tw.setText("Chosen Class: " + chosenClass);
+            }
+        });
+        c4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                chosenClass = "Creativity";
+                tw.setText("Chosen Class: " + chosenClass);
+            }
+        });
+        c5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                chosenClass = "Wisdom";
+                tw.setText("Chosen Class: " + chosenClass);
+            }
+        });
+
     }
 
     public void goToCharSelection(View view) {
+        userLocal.setUserClass(chosenClass);
         Intent intent = new Intent(this, MainPage.class);
         intent.putExtra("USER", userLocal);
         startActivity(intent);
