2d3547f9c5271d7f49ac6ffd113c2df06d3cdd3b
Mehmet Emin Avsar <avsarmehmetemin@gmail.com>
2023-05-30 22:29:48 +0300

    Passing UserLocal to character selection page

--- app/src/main/java/com/example/goaled/PickClass.java
+++ app/src/main/java/com/example/goaled/PickClass.java
@@ -7,16 +7,18 @@ import android.os.Bundle;
 import android.view.View;
 
 public class PickClass extends AppCompatActivity {
-
+    private UserLocal userLocal;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pick_class);
 
+        userLocal = (UserLocal) getIntent().getSerializableExtra("USER");
     }
 
     public void goToCharSelection(View view) {
         Intent intent = new Intent(this, MainPage.class);
+        intent.putExtra("USER", userLocal);
         startActivity(intent);
     }
 }
